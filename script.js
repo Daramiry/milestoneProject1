c0bb9d93c214ecf62446fe197048da512c148f4a
Daramiry <drz0224@gmail.com>
2022-08-18 21:27:44 -0700

    added losing feature

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
-import { updateBird } from "./bird.js"
+import { updateBird, setupBird, getBirdRect } from "./bird.js"
 
-document.addEventListener("keypress", handleStart, { once: true })
+document.addEventListener("keypress", handleStart, { once: true})
 const title = document.querySelector("[data-title]")
+const subtitle = document.querySelector("[data-subtitle]")
 
 let lastTime
+
 function updateLoop(time) {
     if (lastTime == null) {
         lastTime = time
         window.requestAnimationFrame(updateLoop)
         return
     }
-    // console.log(time - lastTime)
     const delta = time - lastTime
     updateBird(delta)
+    if (checkLose()) return handleLose()
     lastTime = time
     window.requestAnimationFrame(updateLoop)
 }
 
+function checkLose() {
+    const birdRect = getBirdRect()
+    const outsideWorld = birdRect.top < 0 || birdRect.bottom > window.innerHeight
+    return outsideWorld
+}
+
 function handleStart() {
-title.classList.add("hide")
-window.requestAnimationFrame(updateLoop)
+    title.classList.add("hide")
+    setupBird()
+    lastTime = null
+    window.requestAnimationFrame(updateLoop)
 }
 
 function handleLose() {
-
+    setTimeout(() => {
+    title.classList.remove("hide")
+    subtitle.classList.remove("hide")
+    subtitle.textContent = "0 Pipes"
+    document.addEventListener("keypress", handleStart, { once: true})
+    }, 1000)
+    
 }
