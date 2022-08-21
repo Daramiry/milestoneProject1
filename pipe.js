dc5cecd8eb152a577479dae099b9a14f51489db2
Daramiry <drz0224@gmail.com>
2022-08-21 11:40:10 -0700

    add pipes

--- pipe.js
+++ pipe.js
@@ -1,32 +1,38 @@
 const HOLE_HEIGHT = 120
-const PIPE_INTERVAL = 1500
 const pipes = []
+const PipeInterval = 1500
+const pipeSpeed = .75
 let timeSinceLastPipe = 0
 
-export function updatePipes() {
+export function updatePipes(delta) {
     timeSinceLastPipe += delta
 
-    if (timeSinceLastPipe < PIPE_INTERVAL) {
-        timeSinceLastPipe -= PIPE_INTERVAL
-        createPipe()
-    }
+    if (timeSinceLastPipe > PipeInterval )
+        timeSinceLastPipe -= PipeInterval
+    createPipe()
 }
+
+pipes.forEach(pipe => {
+    pipe.left = pipe.left - delta * pipeSpeed
+})
+
 function createPipe() {
     const pipeElem = document.createElement("div")
     const topElem = createPipeSegment("top")
     const bottomElem = createPipeSegment("bottom")
-    pipeElem.append(topelem)
+    pipeElem.append(topElem)
     pipeElem.append(bottomElem)
     pipeElem.classList.add("pipe")
     pipeElem.style.setProperty(
         "--hole-top",
-        randomNumberBetween(HOLE_HEIGHT * 1.5,
-        window.innerHeight - HOLE_HEIGHT * 0.5
-        )
+         randomNumberBetween(HOLE_HEIGHT * 1.5,
+         window.innerHeight - HOLE_HEIGHT * 0.5
+         )
     )
     const pipe = {
         get left() {
-            return parseFloat(getComputedStyle(pipeElem).getPropertyValue(--pipe-left))
+            return parseFloat(getComputedStyle(pipeElem).getPropertyPriorityValue
+            ("--pipe-left"))
         },
         set left(value) {
             pipeElem.style.setProperty("--pipe-left", value)
@@ -35,14 +41,13 @@ function createPipe() {
     pipe.left = window.innerWidth
     document.body.append(pipeElem)
     pipes.push(pipe)
-    }
+}
 
 function createPipeSegment(position) {
     const segment = document.createElement("div")
     segment.classList.add("segment", position)
     return segment
 }
-
 function randomNumberBetween(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+// import functions using "./bird.js" file
 import { updateBird, setupBird, getBirdRect } from "./bird.js"
+// import functions from "./pipe.js" file
+import { updatePipes } from "./pipe.js"
 
+// adding eventListener to start up game
 document.addEventListener("keypress", handleStart, { once: true})
 const title = document.querySelector("[data-title]")
 const subtitle = document.querySelector("[data-subtitle]")
 
+// creating a variable to produce and log frame rate/time
 let lastTime
 
 function updateLoop(time) {
@@ -14,17 +19,20 @@ function updateLoop(time) {
     }
     const delta = time - lastTime
     updateBird(delta)
+    updatePipes(delta)
     if (checkLose()) return handleLose()
     lastTime = time
     window.requestAnimationFrame(updateLoop)
 }
 
+// function to check how the game makes users lose
 function checkLose() {
     const birdRect = getBirdRect()
     const outsideWorld = birdRect.top < 0 || birdRect.bottom > window.innerHeight
     return outsideWorld
 }
 
+// game start function
 function handleStart() {
     title.classList.add("hide")
     setupBird()
@@ -32,6 +40,7 @@ function handleStart() {
     window.requestAnimationFrame(updateLoop)
 }
 
+// game lose function
 function handleLose() {
     setTimeout(() => {
     title.classList.remove("hide")
